5547a6b4087533539da81958f44f3cf170e17687
DungTT <dungtrt94@gmail.com>
2020-04-11 16:21:08 +0700

    fix mistake in dog.js file

--- dog.js
+++ dog.js
@@ -1,7 +1,8 @@
-var dog={
-    name:'Mèo',
-    bark:"ụt ịt"
+function dog (name,age){
+    this.name=name;
+    this.age=age;
 }
 dog.prototype.stomach=function(food){
     console.log(food);
 }
+module.exports=dog;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var chalk=require('chalk');
+var dog=require('./dog');
 var student={
     name:"Dung Tran",
     age:20
 }
+var animal=new dog("chó",4);
+console.log(animal.stomach("bone"));
 // student.prototype.dead=false;
 // module.exports=student;
 console.log(chalk.blue(student.name)+ chalk.red(student.age));
