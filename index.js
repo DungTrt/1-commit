ca59e281015af2c6d7e745571b6509b191ce2278
DungTT <dungtrt94@gmail.com>
2020-04-11 14:53:40 +0700

    fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var chalk=require('chalk');
 var student={
     name:"Dung Tran",
     age:20
 }
-student.prototype.dead=false;
-module.exports=student;
+// student.prototype.dead=false;
+// module.exports=student;
+console.log(chalk.blue(student.name)+ chalk.red(student.age));
