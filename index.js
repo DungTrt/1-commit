213ab5df392051bb0c9c8c30ba9299f270afd9df
DungTT <dungtrt94@gmail.com>
2020-04-12 08:20:41 +0700

    remove unnecessary line in index.js

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ var student={
 }
 var animal=new dog("chó",4);
 console.log(animal.stomach("bone"));
-// student.prototype.dead=false;
-// module.exports=student;
 console.log(chalk.blue(student.name)+" "+ chalk.red(student.age));
